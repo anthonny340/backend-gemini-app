801ca81eec76feb1848098f0e6bffde6c904078f
Anthonny Sacheri <tonysolorzano880@gmail.com>
2026-05-26 23:02:12 +0200

    readme: actualizado, docstring metodo basicPrompt agregado

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
+## Requisitos
+
+- Python 3.10 o superior
+- Poetry instalado
+
+Verificar versiones:
+
+```bash
+python --version
+poetry --version
+```
+
 # Dev
 
 1. Clonar repositorio
 2. Crear .env basado en el .env.template
 3. Cambiar las variables de entorno
+4. Instalar dependencia con el comando ```poetry install```
+5. Ejecutar con proyecto con el siguiente comando:
+```
+poetry run fastapi dev app/main.py
+```
--- app/main.py
+++ app/main.py
@@ -18,6 +18,29 @@ def home():
 def basicPrompt(
     content: BasicPrompt
 ):
+    """
+    Genera una respuesta utilizando el modelo Gemini
+    a partir del prompt enviado por el usuario.
+
+    Args:
+        content (BasicPrompt):
+            Contiene el prompt que será procesado por Gemini.
+
+    Returns:
+        dict:
+            Respuesta generada por el modelo.
+
+            Ejemplo:
+            {
+                "success": true,
+                "content": "Respuesta generada..."
+            }
+
+    Raises:
+        Exception:
+            Si ocurre un error durante la generación
+            de contenido con Gemini.
+    """
     gemini = GeminiService()
     response = gemini.generate_content(prompt=content.prompt)
 
